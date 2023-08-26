53e3f1eb48487a56a5ac0697db858d195f7c717e
shahul85198 hameed <shahul85198@gmail.com>
2023-08-26 07:44:56 +0000

    approuter added

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import AppRouter from './AppRouter';
 
 function App() {
   return (
     <div className="App">
       <h1>Ecommerce App</h1>
-     
+     <AppRouter />
     </div>
   );
 }
